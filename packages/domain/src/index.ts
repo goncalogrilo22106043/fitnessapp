cf109018a62163b7025cbf509e220a80db5b9d55
Gonçalo Grilo <100601677+goncalogrilo22106043@users.noreply.github.com>
2026-07-07 21:36:48 +0100

    Delete packages/domain directory

--- packages/domain/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./adaptive-nutrition/engine.js";
-export * from "./adaptive-nutrition/onboarding.js";
-export * from "./adaptive-nutrition/progress.js";
-export * from "./adaptive-nutrition/profile-learning.js";
-export * from "./adaptive-nutrition/scoring.js";
-export * from "./adaptive-nutrition/types.js";
